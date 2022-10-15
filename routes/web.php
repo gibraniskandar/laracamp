52d17cc9d2d57e981c66be994a86573ecde34caf
gibran iskandar <gibran.ramadhan@ticmi.co.id>
2022-10-15 18:53:55 +0700

    sign in google page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,6 +21,9 @@ Route::get('/', function () {
 Route::get('login', function () {
     return view('login');
 })->name('login');
+
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
+Route::get('auth/google/callback', [UserController::class, 'googleCallback'])->name('user.login.google.callback');
 
 Route::get('checkout', function () {
     return view('checkout');
